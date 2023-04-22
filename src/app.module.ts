68fa70e588b667ad7cc1348640ab6dc11dcea326
Sabin Subedi <sabinsubedi125@gmail.com>
2023-04-22 16:31:13 +0545

    minor update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,31 @@
-import { MailerModule } from '@nestjs-modules/mailer';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { CacheModule, CacheStore, Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
+import {
+  CacheModule,
+  CacheStore,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { redisStore } from 'cache-manager-redis-store';
 import { PrismaModule, PrismaService } from 'nestjs-prisma';
-import { join } from 'path';
 import type { RedisClientOptions } from 'redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthGuard } from './auth/auth.guard';
 import { AuthModule } from './auth/auth.module';
 import { CategoriesModule } from './categories/categories.module';
-import {
-  BASE_PATH,
-  EMAIL_SMTP_HOST,
-  EMAIL_SMTP_PASSWORD,
-  EMAIL_SMTP_PORT,
-  EMAIL_SMTP_USER,
-  REDIS_URL,
-} from './constant/env';
+import { REDIS_URL } from './constant/env';
 import { MailModule } from './mail/mail.module';
+import { RequestMiddleware } from './middleware/request.middleware';
 import { RolesGuard } from './roles/roles.guard';
 import { RolesModule } from './roles/roles.module';
 import { RolesService } from './roles/roles.service';
 import { TokenModule } from './token/token.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { UsersModule } from './users/users.module';
-import { BullModule } from '@nestjs/bull';
-import { NestModule } from '@nestjs/common';
-import { MiddlewareConsumer } from '@nestjs/common';
-import { RequestMiddleware } from './middleware/request.middleware';
 
 @Module({
   imports: [
@@ -56,6 +51,10 @@ import { RequestMiddleware } from './middleware/request.middleware';
           port: 6379,
         },
       }),
+    }),
+    EventEmitterModule.forRoot({
+      wildcard: true,
+      delimiter: '.',
     }),
     UsersModule,
     AuthModule,
